4fbfc48e7cde717e902ee60244677ae899e1bcbd
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-10 13:45:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,6 +50,15 @@ def reduce_to_total(source_array,starting_point=0)
     end
     return true
  end
+ 
+  def reduce_to_any_true(array)
+    i = 0 
+    while i < array.length do
+     return true if array[i]==true 
+     i += 1 
+    end
+    return false
+ end
 
 
   
