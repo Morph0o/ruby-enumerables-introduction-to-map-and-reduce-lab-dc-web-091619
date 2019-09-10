c3c2cbdd37b62d8c5615fb491a780e7fd780e959
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-10 13:36:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,7 +36,7 @@ def reduce_to_total(source_array,starting_point=0)
   newarray = [starting_point]
     i = 0 
     while i < source_array.length do
-     newarray.push(source_array[i])
+     newarray += source_array[i]
      i += 1 
     end
   end
